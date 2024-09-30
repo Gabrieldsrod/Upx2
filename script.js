4fb490b3cdf603c0fb3bf7012ae16791508b84f8
luizamantovani <luizamantovani2006@gmail.com>
2024-09-30 18:18:39 -0300

    dropdown

---
+++ script.js
@@ -0,0 +1,51 @@
+const dropdowns = document.querySelectorAll('.dropdown'); // Use querySelectorAll to select all dropdowns
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li'); // Use querySelectorAll to select all li elements
+    const selected = dropdown.querySelector('.selected');
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select-clicked');
+        caret.classList.toggle('caret-rotate');
+        menu.classList.toggle('menu-open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            select.querySelector('span').innerText = option.innerText; // Update the text of the selected option
+
+            select.classList.remove('select-clicked');
+            caret.classList.remove('caret-rotate');
+            menu.classList.remove('menu-open');
+
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            option.classList.add('active');
+        });
+    });
+});
+
+function calcularConsumo() {
+    var potencia = document.querySelector('.select span').innerText.replace('W', ''); // Remove 'W' para obter o valor numérico
+    var tempoMinutos = document.getElementById('tempo').value;
+    var tempoHoras = tempoMinutos / 60; // Converter minutos em horas
+    var consumo = (potencia * tempoHoras) / 1000; // Consumo em kWh
+
+    // Atualizar o consumo em kWh
+    document.querySelector('.consumo').innerText = consumo.toFixed(2) + ' kWh';
+
+    var precoKWh = 0.92; // Preço por kWh em reais
+    var custo = consumo * precoKWh; // Cálculo do custo
+
+    // Atualizar o custo em reais
+    document.querySelector('.custo').innerText = 'R$ ' + custo.toFixed(2);
+
+    // Tornar a seção de resultados visível
+    document.getElementById('resultado').style.display = 'block';
+}
+
+
